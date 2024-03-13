ee41a2d5e7296f812c98e72be0a0f7661da5ffba
srinivasagatla@lyrostech.com <srinivasareddygatla09@gmail.com>
2024-03-13 16:19:24 +0530

    March 12th Updated

--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -2,12 +2,13 @@ import React, { useContext } from "react";
 import { AppContext } from "../../context/appContext.js";
 import products from "./products.json"
 import "./Product.css"
+import AddButton from "./AddButton.js";
 export default function Products() {
   const { cartItems, setCartItems } = useContext(AppContext);
-
-  const addtoCart = (id) => {
-    setCartItems((prev) => ({ ...prev, [id]: (prev[id] ?? 0) + 1 }));
-  };
+  const PATH = process.env.REACT_APP_PATH
+  // const addtoCart = (id) => {
+  //   setCartItems((prev) => ({ ...prev, [id]: (prev[id] ?? 0) + 1 }));
+  // };
 
   return (
     <div className="Product-container">
@@ -19,7 +20,8 @@ export default function Products() {
             <p style={{textAlign:"justify",padding:"10px"}}>{value.desc}</p>
             <div className="priceBtn">
             <div className="priceTxt">₹{value.price}</div>
-            <button className="Product-button" onClick={() => addtoCart(value.id)}>Add</button>
+            <div className="Product-button"><AddButton id={value.id}/></div>
+            {/* <button className="Product-button" onClick={() => addtoCart(value.id)}>Add</button> */}
             </div>
           </div>
         ))}
